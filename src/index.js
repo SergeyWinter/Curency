b23fb0099a9e5fc99e762a7aaf9e9114916bef91
SergeyWinter <32358568+SergeyWinter@users.noreply.github.com>
2023-10-04 13:20:00 +0500

    FFFF

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   <>
-    <div>fffffffff</div>
+    <div>fFFFFFFFF</div>
     <Apps />
   </>
   // </React.StrictMode>,
